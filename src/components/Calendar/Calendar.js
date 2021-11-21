8c4457733d8400b1570a6e8cb6b27485e01f89b5
Phuong tv <phuongphi407@gmail.com>
2021-11-21 23:12:22 +0700

    update code

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -9,19 +9,17 @@ const Calendar = (props) => {
     const listdaysInMonth = (month) => {
         let lists = [{w:[]}];
         const numberDayOfMonth = moment(month, "MM/yyyy").daysInMonth();
-        console.log(month, numberDayOfMonth)
         for (let i = 1; i < numberDayOfMonth + 1; i++) {
-            if(moment(month, "MM/yyyy").date(i-1).format("dd/MM").includes('CN')){
-                lists.push({w:[]})
-            }
             const date={d: moment(month, "MM/yyyy").date(i).format("DD/MM"), T: moment(month, "MM/yyyy").date(i).format("dd/MM"), Y: moment(month, "MM/yyyy").date(i).format("YYYY")}
             lists[lists.length-1].w.push(date)
+            if(moment(month, "MM/yyyy").date(i).format("dd/MM").includes('CN') && i<numberDayOfMonth){ //i<numberDayOfMonth de khang dinh day khong phai nagy cuoi cung cua thang de tao tuan tiep theo
+                lists.push({w:[]})
+            }
         }
         return lists
 
     }
-    console.log("date:", date, moment.weekdays(), listdaysInMonth("12/2021"));
-    console.log(chooseDay)
+    console.log("chooseDay", chooseDay)
     return (
         <div className="calendar">
             <div className="cal-head">THÁNG 11 2021</div>
@@ -34,13 +32,13 @@ const Calendar = (props) => {
                 <div className="grid-item">T bảy</div>
                 <div className="grid-item">C nhật</div>
             </div>
-            {listdaysInMonth("12/2021").map((x, i)=>(
+            {listdaysInMonth("10/2021").map((x, i)=>(
                 <div className="calendar-container">
                     {i===0 && Array.from({length: 7-x.w.length}, (x, i) => i).map((item)=>(
                         <div className="grid-item"></div>
                     ))}
                     {x.w.map((item, i)=>(
-                        <div className="grid-item" onClick={()=>setChooseDay(item)}>{item.d}</div>
+                        <div className="grid-item has-day" onClick={()=>setChooseDay(item)}>{item.d}</div>
                     ))}
                     {i === (listdaysInMonth("12/2021").length-1)&& Array.from({length: 7-x.w.length}, (x, i) => i).map((item)=>(
                         <div className="grid-item"></div>
