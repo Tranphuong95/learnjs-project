de02f546ff8e90fcefc772937531ea809a9f5703
Phuong tv <phuongphi407@gmail.com>
2021-11-22 00:09:23 +0700

    update code

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,28 +1,41 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import "./index.css";
 import moment from 'moment';
 import 'moment/locale/vi';
+
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 const Calendar = (props) => {
-    const [chooseDay, setChooseDay]=useState("")
+    const [chooseDay, setChooseDay] = useState("");
+    const [chooseMonth, setChooseMonth] = useState(moment().format("MM/YYYY"))
     moment.locale('vi');
     const date = moment().startOf("month").format("d/MM/yyyy");
     const listdaysInMonth = (month) => {
-        let lists = [{w:[]}];
+        console.log(month)
+        let lists = [{ w: [] }];
         const numberDayOfMonth = moment(month, "MM/yyyy").daysInMonth();
         for (let i = 1; i < numberDayOfMonth + 1; i++) {
-            const date={d: moment(month, "MM/yyyy").date(i).format("DD/MM"), T: moment(month, "MM/yyyy").date(i).format("dd/MM"), Y: moment(month, "MM/yyyy").date(i).format("YYYY")}
-            lists[lists.length-1].w.push(date)
-            if(moment(month, "MM/yyyy").date(i).format("dd/MM").includes('CN') && i<numberDayOfMonth){ //i<numberDayOfMonth de khang dinh day khong phai nagy cuoi cung cua thang de tao tuan tiep theo
-                lists.push({w:[]})
+            const date = { d: moment(month, "MM/yyyy").date(i).format("DD/MM"), T: moment(month, "MM/yyyy").date(i).format("dd/MM"), Y: moment(month, "MM/yyyy").date(i).format("YYYY") }
+            lists[lists.length - 1].w.push(date)
+            if (moment(month, "MM/yyyy").date(i).format("dd/MM").includes('CN') && i < numberDayOfMonth) { //i<numberDayOfMonth de khang dinh day khong phai nagy cuoi cung cua thang de tao tuan tiep theo
+                lists.push({ w: [] })
             }
         }
         return lists
 
     }
-    console.log("chooseDay", chooseDay)
+    const handleChangeOneMonth=(value)=>{
+        setChooseMonth(moment(chooseMonth, "MM/YYYY").add(value, "month").format("MM/YYYY"))
+    }
+    console.log("chooseDay", chooseDay);
+    console.log("chooseMonth:", chooseMonth, moment(chooseMonth, "MM/YYYY").add(-1, "month").format("MM/YYYY"))
     return (
         <div className="calendar">
-            <div className="cal-head">THÁNG 11 2021</div>
+            <div className="cal-head">
+                <div className="arrow-icon" onClick={()=>handleChangeOneMonth(-1)}><ArrowBackIosIcon style={{marginTop:10}}/></div>
+                <div>{chooseMonth}</div>
+                <div className="arrow-icon" onClick={()=>handleChangeOneMonth(1)}><ArrowForwardIosIcon style={{marginTop: 10}}/></div>
+            </div>
             <div className="calendar-container">
                 <div className="grid-item">T hai</div>
                 <div className="grid-item">T ba</div>
@@ -32,15 +45,15 @@ const Calendar = (props) => {
                 <div className="grid-item">T bảy</div>
                 <div className="grid-item">C nhật</div>
             </div>
-            {listdaysInMonth("10/2021").map((x, i)=>(
+            {listdaysInMonth(chooseMonth).map((x, i) => (
                 <div className="calendar-container">
-                    {i===0 && Array.from({length: 7-x.w.length}, (x, i) => i).map((item)=>(
+                    {i === 0 && Array.from({ length: 7 - x.w.length }, (x, i) => i).map((item) => (
                         <div className="grid-item"></div>
                     ))}
-                    {x.w.map((item, i)=>(
-                        <div className="grid-item has-day" onClick={()=>setChooseDay(item)}>{item.d}</div>
+                    {x.w.map((item, i) => (
+                        <div className="grid-item has-day" onClick={() => setChooseDay(item)}>{item.d}</div>
                     ))}
-                    {i === (listdaysInMonth("12/2021").length-1)&& Array.from({length: 7-x.w.length}, (x, i) => i).map((item)=>(
+                    {i === (listdaysInMonth(chooseMonth).length - 1) && Array.from({ length: 7 - x.w.length }, (x, i) => i).map((item) => (
                         <div className="grid-item"></div>
                     ))}
                 </div>
